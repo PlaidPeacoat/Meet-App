75b480533bedf6d99bc1a5f3a8ca38b896211c1e
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2023-04-13 21:16:06 -0400

    4.5

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -21,7 +21,10 @@ class CitySearch extends Component {
     this.setState({
       query: suggestion
     });
+  
+    this.props.updateEvents(suggestion);
   }
+  
   
 
   render() {
@@ -40,7 +43,7 @@ class CitySearch extends Component {
       onClick={() => this.handleItemClicked(suggestion)}
     >{suggestion}</li>
   ))}
-  <li>
+  <li onClick={() => this.handleItemClicked("all")}>
     <b>See all cities</b>
   </li>
 </ul>
