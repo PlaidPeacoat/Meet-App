6cb5fa45a8a39288ce4c6fef2a3244d0dd606934
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2023-04-24 20:53:09 -0400

    implemented input and error alerts

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,53 +1,48 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from "./Alert";
 
 
 class NumberOfEvents extends Component {
-    state = { 
-        noe: 32,
-        errorText: ''
+    constructor() {
+      super();
+      this.state = {
+        query: 32,
+        errorText: "",
+      };
     }
-
-    // componentDidMount() {
-    //     this.setState({ noe: this.props.noe || 32 });
-    // }
-
-    // changeNOE(value) {
-    //     this.setState({ noe: value })
-    // }
-
-    handleInputChanged = (event, props) => {
-        const inputValue = event.target.value;
-        if(inputValue < 0 || inputValue > 32) {
-           this.setState({
-            errorText: 'Please select number from 1 to 32'
-           }) 
-        } else {
-            this.props.updateEvents(null, inputValue);
-            this.setState({
-                noe: inputValue,
-                errorText: ''
-            })
-        }
+  
+    handleInputChanged = (event) => {
+      const value = event.target.value;
+      if (value >= 1 || value <= 32) {
+        this.setState({
+          query: value,
+          errorText: "",
+        });
+        this.props.updateEvents(this.props.selectedCity, value);
+      }
+      if (value < 1 || value > 32) {
+        this.setState({
+          query: value,
+          errorText: "Please enter a valid number",
+        });
+      }
     };
-
+  
     render() {
-        const { noe } = this.state;
-        return (
-            <div className="NumberOfEvents">
-                <h3>Number of Events:</h3>
-                <input
-                className="noe-input"
-                type="number"
-                value={noe}
-                onChange={event => {
-                   // this.changeNOE(event.target.value);
-                   this.handleInputChanged(event);
-                }}
-                >
-                </input>
-            </div>
-        )
+      return (
+        <div className='numberOfEvents'>
+          <input
+            type='number'
+            className='numberOfEvents'
+            min={1}
+            max={32}
+            value={this.state.query}
+            onChange={this.handleInputChanged}
+          />
+          <ErrorAlert className='errorMessage' text={this.state.errorText} />
+        </div>
+      );
     }
-}
-
-export default NumberOfEvents;
+  }
+  
+  export default NumberOfEvents;
