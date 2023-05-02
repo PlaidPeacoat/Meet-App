f728039cb36cf78a4cc540bedcddf5ee729ee7b9
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2023-05-02 07:40:17 -0400

    installed bootstrap, implemented scatterchart

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ import "./nprogress.css";
 import { WarningAlert } from "./Alert";
 import WelcomeScreen from "./WelcomeScreen";
 import { getEvents, extractLocations, checkToken, getAccessToken } from "./api";
-import {
-  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
-} from 'recharts';
+import { ScatterChart } from "recharts";
+import { CartesianGrid, XAxis, YAxis, Tooltip, Scatter } from "recharts";
 
 
 
@@ -119,38 +118,44 @@ class App extends Component {
   };
 
   render() {
-    const { locations, numberOfEvents } = this.state;
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
-      <div className="App">
+      <div className='App'>
         <h1>Meet App</h1>
         <h4>Choose your nearest city</h4>
-        <CitySearch updateEvents={this.updateEvents} locations={locations} />
-        <NumberOfEvents
+        <CitySearch
+          locations={this.state.locations}
           updateEvents={this.updateEvents}
-          numberOfEvents={numberOfEvents}
-        />       
-         <h4>Events in each city</h4>
-
-         <ScatterChart
-          width={400}
-          height={400}
-          margin={{
-            top: 20, right: 20, bottom: 20, left: 20,
-          }}
-        >
-          <CartesianGrid />
-          <XAxis type="category" dataKey="city" name="city" />
-          <YAxis type="number" dataKey="number" name="number of events" unit="kg" />
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter data={this.getData} fill="#8884d8" />
-        </ScatterChart>
+        />
+        <NumberOfEvents
+          selectedCity={this.state.selectedCity}
+          query={this.state.eventCount}
+          updateEvents={this.updateEvents}
+        />
+        <h4>Events in each city</h4>
+        <ScatterChart
+        width={400}
+        height={400}
+        margin={{
+          top: 20, right: 20, bottom: 20, left: 20,
+        }} >
+        <CartesianGrid />
+        <XAxis type="category" dataKey="city" name="city" />
+        <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter data={this.getData()} fill="#8884d8" />
+      </ScatterChart>
         <WarningAlert text={this.state.warningText} />
+        
         <EventList events={this.state.events} />
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken(); }}
+        />
       </div>
     );
-  };
+  }
 }
-
 
 export default App;
